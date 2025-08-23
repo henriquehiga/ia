913d32f47d7a8174cd24a5790c07b7fba4db5f81
henriquehiga <codehiga@gmail.com>
2025-08-23 16:56:10 -0300

    feat: tools muito simples para entender funcionamento e integração

--- lm_studio_mcp/main.py
+++ lm_studio_mcp/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from lm-studio-mcp!")
+import datetime
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("teste")
+
+
+@mcp.tool(
+    name="dobrar_numero_function",
+    description="Tool para dobrar um número recebido. A função recebe apenas o número.",
+)
+def dobrar_numero(numero: int) -> int:
+    return numero * 2
+
+
+@mcp.tool(
+    name="data_atual_function",
+    description="Essa função retorna o valor correto da data atual. Não recebe nenhum parâmetro, retorna o tipo datetime do python."
+)
+def resgatar_data_atual() -> str:
+    return datetime.date.today().isoformat()
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport='stdio')
